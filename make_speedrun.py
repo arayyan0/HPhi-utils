79cf548629b6841ddab547954f3751c2d30eba4e
arayyan0 <a.rayyan@mail.utoronto.ca>
2022-02-22 12:24:44 -0500

    speedrun can now do SSF if you select output mode is 'correlation'

---
+++ make_speedrun.py
@@ -0,0 +1,68 @@
+import sys
+
+# code should be run from within HPhi-utils
+filename = 'speedrun.sh'
+HPhi_build = '/Users/ahmed/Documents/University/PhD/Research/General/HPhi/HPhi.build/'
+output_folder = 'out'
+
+what_simulation = 'SSF'  #(either 'SSF' or 'simple')
+what_computer  = 'home' #(either 'home' or 'cluster')
+
+#-------------------------------beginning of bash script
+# preamble of bash scripts
+f = open(filename, 'w')
+f.write('#!/bin/bash\n')
+f.write('\n')
+
+# writing function shortcut for HPhi
+f.write('HPhiSC () {\n')
+f.write(f'  command {HPhi_build}src/HPhi $1 $2\n')
+f.write('}\n')
+f.write('\n')
+
+# edit prepare standard file
+f.write(f'vim src/prepare_standard.py\n')
+f.write(f'python3 src/prepare_standard.py\n')
+f.write('\n')
+
+#get into output folder
+f.write(f'cd {output_folder}\n')
+#run in dry standard mode
+f.write(f'HPhiSC -sdry stan.in\n')
+#get out back into HPhi-utils
+f.write(f'cd ..\n')
+f.write('\n')
+
+# append to standard file
+f.write(f'vim src/append_to_standard.py\n')
+f.write(f'python3 src/append_to_standard.py\n')
+f.write('\n')
+
+#get into output folder
+f.write(f'cd {output_folder}\n')
+#run in expert mode
+f.write(f'HPhiSC -e namelist.def\n')
+f.write('\n')
+
+if what_simulation == 'SSF':
+    #append to geometry.dat
+    f.write(f'python3 ../src/ssf_post.py\n')
+    #run greenr2k to calculate reciprocal lattice properties
+    f.write(f'{HPhi_build}tool/greenr2k namelist.def geometry.dat\n')
+    f.write('\n')
+    #create gp_script.gp
+    g = open(output_folder+'/gp_script.gp', 'w')
+    g.write('load "kpath.gp"\n')
+    g.write('plot "output/zvo_corr_eigen0.dat" u 1:12 w l\n')
+    g.write('pause -1 "Hit any key to continue"\n')
+    g.close()
+
+    if what_computer == 'home':
+        #plot the kpath
+        f.write(f'gnuplot gp_script.gp\n')
+        f.write('\n')
+
+#get out back into HPhi-utils
+f.write(f'cd ..\n')
+f.write('\n')
+f.close()
