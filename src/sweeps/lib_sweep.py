403e789e7883a018d9b53dff852414ae847cd7b4
arayyan0 <a.rayyan@mail.utoronto.ca>
2022-03-14 10:14:48 -0400

    minor changes

--- src/sweeps/lib_sweep.py
+++ src/sweeps/lib_sweep.py
@@ -24,7 +24,7 @@ class ComputerPresets:
 
         self.add_computer(      'laptop',               hphi_laptop, False,  4)
         self.add_computer('niagara_gpfs', '/gpfs/fs0/'+hphi_niagara,  True, 40)
-        self.add_computer(     'niagara',              hphi_niagara,  True, 40)
+        self.add_computer('niagara_gpfs', '/gpfs/fs0/'+hphi_niagara,  True, 40)
 
 @dataclass
 class SLURMHelper:
@@ -47,7 +47,8 @@ class SLURMHelper:
            raise SystemExit
     #
     def create_local_sim_commands(self):
-        self.hphi_command = f'srun' if self.computer_settings.mpiQ else ''
+        #may need to fix this later on and change 1 to (tasks per point. for now, its 1)
+        self.hphi_command = f'srun -n 1' if self.computer_settings.mpiQ else ''
 
     def create_submit_script_texts(self):
         slurm_settings = ''
@@ -65,7 +66,7 @@ class SLURMHelper:
 
         parallel_command  = ''
         parallel_command += 'parallel --delay 0.1 '
-        parallel_command += '--jobs $SLURM_NTASKS_PER_NODE ' if self.Ntasksperpoint > 1 else ''
+        parallel_command += '--jobs $SLURM_NTASKS_PER_NODE '
         parallel_command += '--sshloginfile ./node_list_${SLURM_JOB_ID} --workdir $PWD ' \
                             if self.Nnodes > 1 else ''
         parallel_command += '--env OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK '
